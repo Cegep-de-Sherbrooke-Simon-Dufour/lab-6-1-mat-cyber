5d239350916144cf40a444da4869eff95f94c178
Mathieu <mrousseau9@icloud.com>
2023-06-07 15:14:44 -0400

    Fragments

---
+++ app/src/main/java/com/example/lab/ListUserFragment.java
@@ -0,0 +1,62 @@
+package com.example.lab;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListUserFragment extends Fragment {
+
+    UserAdapter userAdapter;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_list_user, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.add_btn).setOnClickListener(v -> {
+            Navigation.findNavController(view).navigate(R.id.action_listUserFragment_to_addUserFragment);
+        });
+
+        UserListViewModel viewModel = new ViewModelProvider(requireActivity()).get(UserListViewModel.class);
+        RecyclerView recyclerView = view.findViewById(R.id.users_list);
+
+        userAdapter = new UserAdapter(new RecyclerCallback<User>() {
+            @Override
+            public void returnValue(User object) {
+                viewModel.removeUser(object);
+            }
+        });
+
+        recyclerView.setAdapter(userAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+
+        viewModel.getUsers().observe(getViewLifecycleOwner(), new Observer<List<User>>() {
+            @Override
+            public void onChanged(List<User> users) {
+                userAdapter.submitList(new ArrayList<User>(users));
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/lab/MainActivity.java
+++ app/src/main/java/com/example/lab/MainActivity.java
@@ -23,50 +23,9 @@ import dagger.hilt.android.AndroidEntryPoint;
 @AndroidEntryPoint
 public class MainActivity extends AppCompatActivity {
 
-    UserAdapter userAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        UserListViewModel viewModel = new ViewModelProvider(this).get(UserListViewModel.class);
-
-        RecyclerView recyclerView = findViewById(R.id.users_list);
-        FloatingActionButton addBtn = findViewById(R.id.add_btn);
-
-        userAdapter = new UserAdapter(new RecyclerCallback<User>() {
-            @Override
-            public void returnValue(User object) {
-                viewModel.removeUser(object);
-            }
-        });
-        recyclerView.setAdapter(userAdapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        viewModel.getUsers().observe(this, new Observer<List<User>>() {
-            @Override
-            public void onChanged(List<User> users) {
-                userAdapter.submitList(new ArrayList<User>(users));
-            }
-        });
-
-        ActivityResultLauncher<Intent> getUser = registerForActivityResult(
-                new ActivityResultContracts.StartActivityForResult(),
-                new ActivityResultCallback<ActivityResult>() {
-                    @Override
-                    public void onActivityResult(ActivityResult result) {
-                        if(result.getResultCode() != 0){
-                            Intent data = result.getData();
-                            viewModel.addUser(new User(data.getStringExtra("name"), data.getStringExtra("email")));
-                        }
-                    }
-                }
-        );
-
-
-        addBtn.setOnClickListener(v -> {
-            getUser.launch(new Intent(this, AddUserActivity.class));
-        });
-
     }
 }
